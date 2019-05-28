0cec4a5b01d6cf2b4963c0cb818f52440df67bba
kjy <tore004400@gmail.com>
2019-05-28 17:33:07 +0900

    branch

---
+++ src/iducs/java/b/Lion.java
@@ -0,0 +1,19 @@
+package iducs.java.b;
+
+public class Lion extends Mammal {
+
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+		System.out.println("³×¹ß·Î °È°í, ¶Ú´Ù");
+
+	}
+
+	@Override
+	public void sound() {
+		// TODO Auto-generated method stub
+		System.out.println("roaring : ¾ß¿Ë ");
+
+	}
+
+}
